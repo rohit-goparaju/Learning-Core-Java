ec85ce42ce2345ca4d22efe57edb57348d539766
Rohit Goparaju <rohitgoparaju.r96@gmail.com>
2025-08-04 18:24:41 +0530

    add log4j2 and slf4j

--- learning-core-java/src/main/java/com/myProjects/learning_core_java/App.java
+++ learning-core-java/src/main/java/com/myProjects/learning_core_java/App.java
@@ -1,13 +1,18 @@
 package com.myProjects.learning_core_java;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
+	public static Logger log = LoggerFactory.getLogger(App.class);
+	
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	log.info("Hello world");
     }
 }
