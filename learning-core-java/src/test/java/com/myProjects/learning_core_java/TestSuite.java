0f45baf6090e5f2c2ccaed35e3c3178c8a0d4f6d
Rohit Goparaju <rohitgoparaju.r96@gmail.com>
2025-08-13 12:32:38 +0530

    add test and test suite for App.java

---
+++ learning-core-java/src/test/java/com/myProjects/learning_core_java/TestSuite.java
@@ -0,0 +1,12 @@
+package com.myProjects.learning_core_java;
+
+import org.junit.platform.suite.api.SelectClasses;
+import org.junit.platform.suite.api.Suite;
+
+@Suite
+@SelectClasses({
+	AppTest.class
+})
+public class TestSuite {
+	
+}
